dfd063f5f66ebdcb0fa079c587ea75d1dec290b0
Jorge Cutiva <jcutiva1214@gmail.com>
2024-05-11 18:20:33 -0500

    Implement React Router and the component was created when the page is not found

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Root from './routes/Root'
+import ErrorPage from './routes/ErrorPage'
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root/>,
+    errorElement: <ErrorPage/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 )
